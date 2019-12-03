05c9cb3daceed297f1e4458ab512c0c777854d80
Maksuedson <maksuedson@hotmail.com>
2019-12-02 22:18:04 -0300

    Camada Chess e imprimindo o tabuleir

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,12 @@
 package aplication;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
-
 }
